e4b98a4d5b5490d8c48117528590cbe5b7f90258
Dylan <dxa4481@rit.edu>
2020-08-06 11:23:03 -0700

    urg

--- bfs.py
+++ bfs.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     org_id = args.org
     visited, info = bfs_search(org_id, base_id)
 
-    print("\n\n~~~~~~~{} can move laterally to the following identities~~~~~~~~~~~".format(base_id))
+    print("\n\n~~~~~~~{} can move laterally to the following identities ~~~~~~~~~~~".format(base_id))
     for service_account in visited:
         if service_account != base_id:
             print("{} from project {}".format(service_account, info[service_account]))
